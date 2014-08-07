bde7963d6f3b61928ff4865bb943e8ccf1e52019
Fredrik Mundt <famthegeek95@gmail.com>
2014-08-07 23:14:45 +0200

    Removed empty lines

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var static = require('node-static');
-
 var fileServer = new static.Server('./public', { cache: 3600 });
-
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         fileServer.serve(request, response);
