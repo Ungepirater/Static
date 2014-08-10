e6568bd833198013f5228feeb840b2eeecbc9276
Fredrik Mundt <famthegeek95@gmail.com>
2014-08-10 18:46:32 +0200

    Fixed headers and removed console.log

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var static = require('node-static');
 var fileServer = new static.Server('./public', { cache: 3600 });
 require('http').createServer(function (request, response) {
-	response.header("Access-Control-Allow-Origin", "*");
-	response.header("Access-Control-Allow-Headers", "X-Requested-With");
+	response.setHeader("Access-Control-Allow-Origin", "*");
+	response.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
 	request.addListener('end', function () {
-		console.log(request);
 		fileServer.serve(request, response);
 	}).resume();
 }).listen(3002);
