09b5fe689f90b861cec60fcfdc829afef773c900
Fredrik Mundt <famthegeek95@gmail.com>
2014-08-10 18:42:35 +0200

    Added Access-Control-Allow-Origin headers

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var static = require('node-static');
 var fileServer = new static.Server('./public', { cache: 3600 });
 require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-    	console.log(request);
-        fileServer.serve(request, response);
-    }).resume();
+	response.header("Access-Control-Allow-Origin", "*");
+	response.header("Access-Control-Allow-Headers", "X-Requested-With");
+	request.addListener('end', function () {
+		console.log(request);
+		fileServer.serve(request, response);
+	}).resume();
 }).listen(3002);
