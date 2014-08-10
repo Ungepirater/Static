9b703a3fb3d2373eee76b19af207e1cc6aa0baf0
Fredrik Mundt <famthegeek95@gmail.com>
2014-08-10 18:37:37 +0200

    Installed fontawesome

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var static = require('node-static');
 var fileServer = new static.Server('./public', { cache: 3600 });
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
+    	console.log(request);
         fileServer.serve(request, response);
     }).resume();
 }).listen(3002);
